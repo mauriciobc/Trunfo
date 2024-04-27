0f4bfcd005a6adf190fc0efc0be45de4df1acc38
mauriciobc <mauriciobc@users.noreply.github.com>
2024-04-27 20:11:35 +0000

    Refactor game class

--- game.py
+++ game.py
@@ -1,36 +1,75 @@
 from random import shuffle
-from card_data import CARDS
+
+class Card:
+    def __init__(self, attributes):
+        self.attributes = attributes
+
+    def get_attribute(self, category):
+        return self.attributes[category]
+
+
+class Deck:
+    def __init__(self, name, cards):
+        self.name = name
+        self.cards = cards
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def draw(self, num_cards):
+        return [self.cards.pop() for _ in range(num_cards)]
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def add_to_hand(self, cards):
+        self.hand.extend(cards)
+
+    def play_card(self, index):
+        return self.hand.pop(index)
+
 
 class Game:
-  def __init__(self, deck_name, num_players):
-    self.deck_name = deck_name
-    self.players = [[] for _ in range(num_players)]  # Player hands
-    self.discard_pile = []
-    self.current_category = None
-    self.deal_cards(CARDS)  # Replace CARDS with your card data
+    def __init__(self, deck, num_players):
+        self.deck = deck
+        self.players = [Player(f"Player {i+1}") for i in range(num_players)]
+        self.discard_pile = []
+        self.current_category = None
+        self.winners = []
+        self.initialize_game()
 
-  def deal_cards(self, card_data):
-    shuffle(card_data)
-    for player in self.players:
-      player.extend(card_data[:5])  # Deal 5 cards to each player
-      del card_data[:5]  # Remove dealt cards from deck
-      
+    def initialize_game(self):
+        self.deck.shuffle()
+        for player in self.players:
+            player.add_to_hand(self.deck.draw(5))
 
-  def play_round(self, player_index, category):
-    self.current_category = category
-    played_cards = []
-    for player in self.players:
-      card = player.pop(player_index)  # Player plays chosen card
-      played_cards.append(card)
-    winner_index = self.find_winner(played_cards, category)
-    self.discard_pile.extend(played_cards)
-    return winner_index
+    def play_round(self, player_index, category):
+        played_cards = []
+        for player in self.players:
+            card = player.play_card(player_index)
+            played_cards.append(card)
+        winner_index = self.find_winner(played_cards, category)
+        self.discard_pile.extend(played_cards)
+        self.update_scores(winner_index)
+        return winner_index
 
-  def find_winner(self, cards, category):
-    winning_card = max(cards, key=lambda card: card[category])
-    return cards.index(winning_card)
+    def find_winner(self, cards, category):
+        winning_card = max(cards, key=lambda card: card.get_attribute(category))
+        return cards.index(winning_card)
 
-# Example usage
-game = Game("Dinosaurs", 2)
-winner_index = game.play_round(0, "frill_length")  # Player 1 plays first, category: frill_length
-print(f"Player {winner_index + 1} wins the round!")
+    def update_scores(self, winner_index):
+        self.players[winner_index].score += len(self.players)
+
+    def check_for_end_of_game(self):
+        active_players = [player for player in self.players if len(player.hand) > 0]
+        if len(active_players) <= 1:
+            self.determine_winner()
+            return True
+        return False
+
+    def determine_winner(self):
+        max_score = max([player.score for player in self.players])
+        winners = [player for player in self.players if player.score == max
